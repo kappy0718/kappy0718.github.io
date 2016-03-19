e22b4db7af5d1dc065747b66a55fbd131b2f6981
shunsuke oike <tt330b@gmail.com>
2016-03-20 00:03:34 +0900

    make kappy and startup object

--- kappy/js/topJs/startup.js
+++ kappy/js/topJs/startup.js
@@ -1,3 +1,4 @@
+//for Masonry
 window.onload = function(){
     new Masonry("body",{
         itemSelector: " .item",
@@ -5,3 +6,39 @@ window.onload = function(){
         gutter: 4
     });
 }
+// for jQuery
+$(document).ready(function(){
+    console.log("jQuery start");
+
+    //load startup
+    kappy.startup = (function(){
+        var loadScript = function(src) {
+            var script = document.createElement( "script" );
+            var d = new $.Deferred;
+
+            script.type = "text/javascript";
+            script.src = src;
+            script.onload = function( e )
+            {
+                console.log("loaded event : " + src);
+                d.resolve();
+            }
+            var firstScript = document.getElementsByTagName( "script" )[0];
+            firstScript.parentNode.insertBefore( script, firstScript );
+            document.body.appendChild( script );
+            return d.promise();
+        }
+        return {loadScript: loadScript}
+    })();
+
+    // load src
+    var src = "js/topJs/diaryMaker.js";
+    kappy.startup.loadScript(src)
+        .then(function(){
+            console.log("loaded deferred : " + src);
+    });;
+});
+
+
+
+
