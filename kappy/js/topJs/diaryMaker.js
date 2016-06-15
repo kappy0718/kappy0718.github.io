fe1a789085e1f1c540dc530e802ad581345b2bed
大池瞬介 <tt330b@gmail.com>
2016-06-15 21:39:13 +0900

    add eating video diary

--- kappy/js/topJs/diaryMaker.js
+++ kappy/js/topJs/diaryMaker.js
@@ -4,6 +4,7 @@ kappy.diaryMaker = new (function(){
 
     this.DIARY_SOURCE = [
         "diary/201603/json/kappy_20160320.json",
+        "diary/201606/json/kappy_20160615.json",
         "diary/201606/json/kappy_20160614.json",
         "diary/201606/json/kappy_20160612.json",
         "diary/201606/json/kappy_20160613.json",
